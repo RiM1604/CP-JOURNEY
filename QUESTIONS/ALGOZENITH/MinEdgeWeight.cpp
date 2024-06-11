5d6e11260dba5df0526cb7df98eec984418fd8ed
RiM1604 <irnmadhunala123@gmail.com>
2024-06-12 00:03:33 +0530

    Automatic commit 2024-06-12 00:03:32

---
+++ QUESTIONS/ALGOZENITH/MinEdgeWeight.cpp
@@ -0,0 +1,174 @@
+// Date: 11-06-2024
+// Start Time: 19:16:12
+// End Time  : 20:14:00
+// Time Taken: 57 minutes
+// Author: RiM1604
+// QUESTION LINK: ..
+// Rating: Medium
+// Description: AZ problem
+// Solved: Yes
+// Learning: Still forgot again that for getting rank you need to do find(x) for rank.
+
+/****************************************************Pre Processor***************************************************/
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#ifdef LOCAL
+#include "./lib/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+/****************************************************Definition***************************************************/
+#define endl '\n'
+#define pb push_back
+// #define int long long
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+
+/**************************************************Global Variables*************************************************/
+const ll MAXM = 1e5;
+int mod = 1e9 + 7;
+int INF = 1e9 + 5;
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+double PI = acos(-1);
+vector<long> factors[MAXM + 5];
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+/**************************************************Utility Functions***********************************************/
+void init()
+{
+    for (ll i = 1; i <= MAXM; i++)
+    {
+        for (ll j = i; j <= MAXM; j += i)
+        {
+            factors[j].pb(i);
+        }
+    }
+}
+
+int bin_pow(int base, int pow)
+{
+    int ans = 1;
+    while (pow)
+    {
+        if (pow & 1)
+        {
+            ans = 1LL * ans * base % mod;
+        }
+        base = 1LL * base * base % mod;
+        pow /= 2;
+    }
+    return ans;
+}
+/***************************************************Main Function**************************************************/
+struct UnionFind
+{
+    int n, set_size, *parent, *rank;
+    UnionFind() {}
+    UnionFind(int a)
+    {
+        n = set_size = a;
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+    int find(int x)
+    {
+        if (x != parent[x])
+        {
+            return parent[x] = find(parent[x]);
+        }
+        return x;
+    }
+
+    void merge(int x, int y)
+    {
+        int xroot = find(x), yroot = find(y);
+        if (xroot != yroot)
+        {
+            if (rank[xroot] >= rank[yroot])
+            {
+                parent[yroot] = xroot;
+                rank[xroot] += rank[yroot];
+            }
+            else
+            {
+                parent[xroot] = yroot;
+                rank[yroot] += rank[xroot];
+            }
+            set_size -= 1;
+        }
+    }
+    void reset()
+    {
+        set_size = n;
+        for (int i = 1; i <= n; i++)
+        {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+};
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<pair<int, pair<int, int>>> edgelist;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edgelist.pb({c, {a, b}});
+    }
+    UnionFind uf(n + 1);
+    sort(edgelist.begin(), edgelist.end());
+    reverse(edgelist.begin(), edgelist.end());
+    // for (int i = 0; i < n - 1; i++)
+    // {
+    //     cout << edgelist[i].first << edgelist[i].second.first << edgelist[i].second.second << " ";
+    // }
+    // cout << endl;
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        int xroot = uf.find(edgelist[i].second.first);
+        int yroot = uf.find(edgelist[i].second.second);
+        ans += uf.rank[xroot] * uf.rank[yroot] * edgelist[i].first;
+        uf.merge(edgelist[i].second.first, edgelist[i].second.second);
+    }
+    cout << ans << endl;
+}
+
+signed main()
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+#ifdef LOCAL
+        std::cerr << "Case # " << i << endl;
+        std::cout << "Case #" << i << endl;
+#endif
+        solve();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
