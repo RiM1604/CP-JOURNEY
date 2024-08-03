adff89dda2a48baac299c6bdd5ab0e249043da9f
RiM1604 <irnmadhunala123@gmail.com>
2024-08-03 17:36:42 +0530

    Automatic commit 2024-08-03 17:36:41

---
+++ QUESTIONS/CF/AUGUST/Team.cpp
@@ -0,0 +1,149 @@
+// Date: 03-08-2024
+// Start Time: 14:19:33
+// End Time  : 14:42:16
+// Time Taken: 22 minutes
+// Author: RiM1604
+// QUESTION LINK:
+// Rating:
+// Description:
+// Solved:
+// Learning:
+
+/****************************************************Pre Processor***************************************************/
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+#ifdef LOCAL
+#include "./lib/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+/****************************************************Definition***************************************************/
+#define endl '\n'
+#define pb push_back
+#define F first
+#define S second
+// #define int long long
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+
+/**************************************************Global Variables*************************************************/
+const ll MAXM = 1e5;
+int mod = 1e9 + 7;
+int INF = 1e9 + 5;
+long long INFF = 1000000000000000005LL;
+double EPS = 1e-9;
+double PI = acos(-1);
+vector<long> factors[MAXM + 5];
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+
+/**************************************************Utility Functions***********************************************/
+void init()
+{
+    for (ll i = 1; i <= MAXM; i++)
+    {
+        for (ll j = i; j <= MAXM; j += i)
+        {
+            factors[j].pb(i);
+        }
+    }
+}
+
+int bin_pow(int base, int pow)
+{
+    int ans = 1;
+    while (pow)
+    {
+        if (pow & 1)
+        {
+            ans = 1LL * ans * base % mod;
+        }
+        base = 1LL * base * base % mod;
+        pow /= 2;
+    }
+    return ans;
+}
+/***************************************************Main Function**************************************************/
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    int ans;
+    if (n > m + 1)
+    {
+        cout << -1 << endl;
+    }
+    else if (m > 2 * (n + 1))
+    {
+        cout << -1 << endl;
+    }
+    else
+    {
+        int temp_n = n, temp_m = m;
+        if (temp_n < temp_m)
+        {
+            while ((temp_n != temp_m) && (temp_n || temp_m))
+            {
+                if (temp_n > 0 && temp_m > 0)
+                {
+                    cout << 110;
+                    temp_n--;
+                    temp_m -= 2;
+                }
+                else if (temp_n > 0)
+                {
+                    cout << 0;
+                    temp_n--;
+                }
+                else if (temp_m > 0)
+                {
+                    cout << 11;
+                    temp_m -= 2;
+                }
+            }
+            while ((temp_n > 0 || temp_m > 0))
+            {
+                if (temp_n > 0)
+                {
+                    cout << 0;
+                    temp_n--;
+                }
+                else if (temp_m > 0)
+                {
+                    cout << 1;
+                    temp_m--;
+                }
+            }
+            cout << endl;
+        }
+    }
+}
+
+signed main()
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+    for (int i = 1; i <= t; i++)
+    {
+#ifdef LOCAL
+        std::cerr << "Case # " << i << endl;
+        std::cout << "Case #" << i << endl;
+#endif
+        solve();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+    return 0;
+}
